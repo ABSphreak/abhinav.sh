4d5bc475cd7422304339925a8c758e56b261a2be
ᴀʙʜɪɴᴀᴠ sʜᴀʀᴍᴀ✩ <absphreak@outlook.com>
2020-07-19 20:07:52 +0530

    ➕ Adding Data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,30 @@
 require(`dotenv`).config({
   path: `.env`,
-})
+});
 
-const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE;
 
 module.exports = {
   siteMetadata: {
     // You can overwrite values here that are used for the SEO component
     // Of course you can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    // Used for the title template on pages other than the index site
+    siteTitle: `Abhinav Sharma`,
+    // Default title of the page
+    siteTitleAlt: `Abhinav Sharma`,
+    // Can be used for e.g. JSONLD
+    siteHeadline: `Abhinav Sharma | Portfolio`,
+    // Will be used to generate absolute URLs for og:image etc.
+    siteUrl: `https://abhinav.sh`,
+    // Used for SEO
+    siteDescription: `I am a developer, I like to create stuff.`,
+    // Will be set on the <html /> tag
+    siteLanguage: `en`,
+    // Used for og:image and must be placed inside the `static` folder
+    siteImage: `/banner.jpg`,
+    // Twitter Handle
+    author: `@ABSphreak`,
   },
   plugins: [
     {
@@ -58,4 +73,4 @@ module.exports = {
       },
     },
   ].filter(Boolean),
-}
+};
